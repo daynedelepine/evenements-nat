97c8431c1e8f8cd6a9e499a7baf80a373828cd64
daynedelepine <dayne.de.lepine@gmail.com>
2025-11-16 11:57:11 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+// Firebase Configuration - REMPLACEZ PAR VOS VRAIES VALEURS
+// Obtenez ces valeurs depuis https://console.firebase.google.com
+
+const firebaseConfig = {
+    apiKey: "REPLACE_WITH_YOUR_API_KEY",
+    authDomain: "your-project.firebaseapp.com",
+    projectId: "your-project-id",
+    storageBucket: "your-project.appspot.com",
+    messagingSenderId: "your-messaging-sender-id",
+    appId: "your-app-id"
+};
+
+// Admin Password
+const ADMIN_PASSWORD = "admin123";
